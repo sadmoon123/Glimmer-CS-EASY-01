9c378cb3e69c2518b8846586c7e7eb8d1ac64311
Haixin Yang <2024090903023@std.uestc.edu.cn>
2024-09-21 20:05:15 +0800

    Add files via upload

---
+++ GCD.c
@@ -0,0 +1,14 @@
+int main()
+{
+    int m,n,t;
+    printf("请输入两个数,其中0<m,n<2^31,中间用逗号隔开:");
+    scanf("%d,%d",&m,&n);
+    t=m%n;
+    while(t!=0)
+    {
+        m=n;
+        n=t;
+        t=m%n;
+    }
+    printf("最大公约数是：%d",n);
+}
